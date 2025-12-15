8392a834103c61b871c2d58ec442b85f97b612d7
Yunn Yamone Aung <yoonie@Yoonies.local>
2025-12-16 00:30:59 +0700

    Fix gitignore and remove sensitive files

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"uYlmHXsDCW6qifX1VejaAguTyspY0h3EbHgQEz7L+80=\"\n}"
--- .next/dev/static/chunks/app_page_jsx_6bece898._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_202fe133._.js",
-  "static/chunks/node_modules_3ea52181._.js"
-],
-    source: "dynamic"
-});
